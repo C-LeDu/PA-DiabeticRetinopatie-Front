e5966635d244608dfbee0731b6687385793c1b56
Charlotte <le.du.charlotte@gmail.com>
2019-06-04 20:42:59 +0200

    analyze component part 2

--- src/app/components/analysis/analysis.component.ts
+++ src/app/components/analysis/analysis.component.ts
@@ -9,10 +9,18 @@ export class AnalysisComponent implements OnInit {
   file1: File;
   file2: File;
   analyzeDone = false;
+  loading = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  requestAnalyze() {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+      this.analyzeDone = true;
+    }, 3000);
+  }
 }
--- src/app/components/utils/utils.module.ts
+++ src/app/components/utils/utils.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DropZoneComponent} from './drop-zone/drop-zone.component';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     DropZoneComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ],
   exports: [
     DropZoneComponent,
