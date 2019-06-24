b4499f064c7c1d229c116c89badb5298b97b1a9f
Charlotte <le.du.charlotte@gmail.com>
2019-06-24 23:02:32 +0200

    test link api

--- src/app/services/analysis.service.ts
+++ src/app/services/analysis.service.ts
@@ -10,7 +10,7 @@ export class AnalysisService {
   constructor(private http: HttpClient) { }
 
   public getResult(file: File): Observable<HttpResponse<Blob>> {
-    const uri = 'http://127.0.0.1:5000/predict';
+    const uri = 'https://retinopathy-diabetic-api.appspot.com/predict';
     const formData: FormData = new FormData();
     formData.append('image_file', file, file.name);
     return this.http.post(uri, formData, {observe: 'response', responseType: 'blob'});
